f0b4094596f2a3a526ac52be1a2f7c382a0b885c
Antonio Jose - Frigobyte <github.antonio@frigobyte.com>
2022-04-30 23:42:55 -0300

    Inicio dos testes

---
+++ TreinamentoDLL/FormTestes.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace TreinamentoDLL
+{
+    partial class FormTestes
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConsultaStatusNFe = new System.Windows.Forms.Button();
+            this.btnConsultaSituacao = new System.Windows.Forms.Button();
+            this.groupBoxNFe = new System.Windows.Forms.GroupBox();
+            this.groupBoxNFe.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConsultaStatusNFe
+            // 
+            this.btnConsultaStatusNFe.Location = new System.Drawing.Point(6, 21);
+            this.btnConsultaStatusNFe.Name = "btnConsultaStatusNFe";
+            this.btnConsultaStatusNFe.Size = new System.Drawing.Size(169, 23);
+            this.btnConsultaStatusNFe.TabIndex = 0;
+            this.btnConsultaStatusNFe.Text = "Consulta Status";
+            this.btnConsultaStatusNFe.UseVisualStyleBackColor = true;
+            this.btnConsultaStatusNFe.Click += new System.EventHandler(this.btnConsultaStatusNFe_Click);
+            // 
+            // btnConsultaSituacao
+            // 
+            this.btnConsultaSituacao.Location = new System.Drawing.Point(6, 50);
+            this.btnConsultaSituacao.Name = "btnConsultaSituacao";
+            this.btnConsultaSituacao.Size = new System.Drawing.Size(169, 23);
+            this.btnConsultaSituacao.TabIndex = 1;
+            this.btnConsultaSituacao.Text = "Consulta Situação";
+            this.btnConsultaSituacao.UseVisualStyleBackColor = true;
+            this.btnConsultaSituacao.Click += new System.EventHandler(this.btnConsultaSituacao_Click);
+            // 
+            // groupBoxNFe
+            // 
+            this.groupBoxNFe.Controls.Add(this.btnConsultaStatusNFe);
+            this.groupBoxNFe.Controls.Add(this.btnConsultaSituacao);
+            this.groupBoxNFe.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxNFe.Name = "groupBoxNFe";
+            this.groupBoxNFe.Size = new System.Drawing.Size(301, 309);
+            this.groupBoxNFe.TabIndex = 2;
+            this.groupBoxNFe.TabStop = false;
+            this.groupBoxNFe.Text = "NFe";
+            // 
+            // FormTestes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxNFe);
+            this.Name = "FormTestes";
+            this.Text = "Form Testes";
+            this.Load += new System.EventHandler(this.FormTestes_Load);
+            this.groupBoxNFe.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConsultaStatusNFe;
+        private System.Windows.Forms.Button btnConsultaSituacao;
+        private System.Windows.Forms.GroupBox groupBoxNFe;
+    }
+}
+
---
+++ TreinamentoDLL/FormTestes.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Unimake.Business.DFe.Xml.NFe;
+using Unimake.Security.Platform;
+using Unimake.Business.DFe.Servicos;
+using Unimake.Business.DFe.Servicos.NFe;
+
+namespace TreinamentoDLL
+{
+    public partial class FormTestes : Form
+    {
+        public FormTestes() => InitializeComponent();
+
+        private void FormTestes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConsultaStatusNFe_Click(object sender, EventArgs e)
+        {
+
+            var xml = new ConsStatServ
+            {
+
+                Versao = "4.00",
+                CUF = UFBrasil.AM,
+                TpAmb = TipoAmbiente.Homologacao
+            };
+
+            var configuracao = new Configuracao
+            {
+                TipoDFe = TipoDFe.NFe,
+                TipoEmissao = TipoEmissao.Normal,
+                CertificadoDigital = CertificadoSelecionado
+            };
+
+            var statusServico = new StatusServico(xml, configuracao);
+
+            statusServico.Executar();
+
+            MessageBox.Show(statusServico.Result.CStat + " " + statusServico.Result.XMotivo);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConsultaSituacao_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Certificado Digital
+
+        /// <summary>
+        /// Caminho Completo do Certificado
+        /// </summary>
+        private static string CaminhoCertificadoDigital { get; set; } = @"C:\Dropbox\AGROPAM\AGROPAM 2022.pfx";
+        
+        /// <summary>
+        /// Senha do Certificado
+        /// </summary>
+        private static string SenhaCertificadoDigital { get; set; } = "senhaadescobrir";
+
+        /// <summary>
+        /// Campo para o Certificado Selecionado
+        /// </summary>
+        private static X509Certificate2 CertificadoSelecionadoCampo;
+
+        public static X509Certificate2 CertificadoSelecionado 
+        {
+            get 
+            {
+                if (CertificadoSelecionadoCampo == null) 
+                {
+                    CertificadoSelecionadoCampo = new CertificadoDigital().CarregarCertificadoDigitalA1(CaminhoCertificadoDigital, SenhaCertificadoDigital);
+                }
+
+                return CertificadoSelecionadoCampo;
+            }
+
+            private set => throw new Exception("Não é possível atribuir um certificado digital ! Somente resgate o valor da propriedade que o certificado é definido automaticamente.");
+        }
+
+
+        #endregion
+
+    }
+}
