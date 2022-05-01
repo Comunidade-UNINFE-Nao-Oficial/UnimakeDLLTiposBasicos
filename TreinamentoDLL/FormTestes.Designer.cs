4f717e258faa8e1882b28eaa72da40e9e46c16f6
Antonio Jose - Frigobyte <github.antonio@frigobyte.com>
2022-05-01 16:52:00 -0300

    Ajustes pra facilitar uso do certificado

--- TreinamentoDLL/FormTestes.Designer.cs
+++ TreinamentoDLL/FormTestes.Designer.cs
@@ -32,6 +32,11 @@ namespace TreinamentoDLL
             this.btnConsultaStatusNFe = new System.Windows.Forms.Button();
             this.btnConsultaSituacao = new System.Windows.Forms.Button();
             this.groupBoxNFe = new System.Windows.Forms.GroupBox();
+            this.textBoxArqCert = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxSenhaCert = new System.Windows.Forms.TextBox();
+            this.textBox_resultado = new System.Windows.Forms.TextBox();
             this.groupBoxNFe.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,24 +64,81 @@ namespace TreinamentoDLL
             // 
             this.groupBoxNFe.Controls.Add(this.btnConsultaStatusNFe);
             this.groupBoxNFe.Controls.Add(this.btnConsultaSituacao);
-            this.groupBoxNFe.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxNFe.Location = new System.Drawing.Point(12, 59);
             this.groupBoxNFe.Name = "groupBoxNFe";
             this.groupBoxNFe.Size = new System.Drawing.Size(301, 309);
             this.groupBoxNFe.TabIndex = 2;
             this.groupBoxNFe.TabStop = false;
             this.groupBoxNFe.Text = "NFe";
             // 
+            // textBoxArqCert
+            // 
+            this.textBoxArqCert.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBoxArqCert.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.textBoxArqCert.Location = new System.Drawing.Point(13, 30);
+            this.textBoxArqCert.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxArqCert.Name = "textBoxArqCert";
+            this.textBoxArqCert.Size = new System.Drawing.Size(943, 22);
+            this.textBoxArqCert.TabIndex = 20;
+            this.textBoxArqCert.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBoxArqCert_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 17);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Arquivo do Certificado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(965, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 17);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Senha do Certificado";
+            // 
+            // textBoxSenhaCert
+            // 
+            this.textBoxSenhaCert.Location = new System.Drawing.Point(965, 30);
+            this.textBoxSenhaCert.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxSenhaCert.Name = "textBoxSenhaCert";
+            this.textBoxSenhaCert.Size = new System.Drawing.Size(308, 22);
+            this.textBoxSenhaCert.TabIndex = 22;
+            this.textBoxSenhaCert.UseSystemPasswordChar = true;
+            this.textBoxSenhaCert.TextChanged += new System.EventHandler(this.textBoxSenhaCert_TextChanged);
+            // 
+            // textBox_resultado
+            // 
+            this.textBox_resultado.Location = new System.Drawing.Point(12, 403);
+            this.textBox_resultado.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_resultado.Multiline = true;
+            this.textBox_resultado.Name = "textBox_resultado";
+            this.textBox_resultado.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_resultado.Size = new System.Drawing.Size(1261, 237);
+            this.textBox_resultado.TabIndex = 24;
+            // 
             // FormTestes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1282, 653);
+            this.Controls.Add(this.textBox_resultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxSenhaCert);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxArqCert);
             this.Controls.Add(this.groupBoxNFe);
             this.Name = "FormTestes";
             this.Text = "Form Testes";
             this.Load += new System.EventHandler(this.FormTestes_Load);
             this.groupBoxNFe.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,6 +147,11 @@ namespace TreinamentoDLL
         private System.Windows.Forms.Button btnConsultaStatusNFe;
         private System.Windows.Forms.Button btnConsultaSituacao;
         private System.Windows.Forms.GroupBox groupBoxNFe;
+        private System.Windows.Forms.TextBox textBoxArqCert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSenhaCert;
+        private System.Windows.Forms.TextBox textBox_resultado;
     }
 }
 
--- TreinamentoDLL/FormTestes.cs
+++ TreinamentoDLL/FormTestes.cs
@@ -17,7 +17,18 @@ namespace TreinamentoDLL
 {
     public partial class FormTestes : Form
     {
-        public FormTestes() => InitializeComponent();
+        public FormTestes() 
+        {
+
+            InitializeComponent();
+
+            this.textBoxArqCert.Text = @"C:\Temp\CertificadoA1.pfx";
+            CaminhoCertificadoDigital = this.textBoxArqCert.Text;
+
+            this.textBoxSenhaCert.Text = "";
+            SenhaCertificadoDigital = this.textBoxSenhaCert.Text;
+
+        } 
 
         private void FormTestes_Load(object sender, EventArgs e)
         {
@@ -25,33 +36,44 @@ namespace TreinamentoDLL
         }
 
         /// <summary>
-        /// 
+        /// Consulta Status da NFe
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void btnConsultaStatusNFe_Click(object sender, EventArgs e)
         {
+            this.textBox_resultado.Text = "";
 
-            var xml = new ConsStatServ
+            try
             {
 
-                Versao = "4.00",
-                CUF = UFBrasil.AM,
-                TpAmb = TipoAmbiente.Homologacao
-            };
+                var xml = new ConsStatServ
+                {
+                    Versao = "4.00",
+                    CUF = UFBrasil.AM,
+                    TpAmb = TipoAmbiente.Homologacao
+                };
 
-            var configuracao = new Configuracao
+                var configuracao = new Configuracao
+                {
+                    TipoDFe = TipoDFe.NFe,
+                    TipoEmissao = TipoEmissao.Normal,
+                    CertificadoDigital = CertificadoSelecionado
+                };
+
+                var statusServico = new StatusServico(xml, configuracao);
+
+                statusServico.Executar();
+
+                //MessageBox.Show(statusServico.Result.CStat + " " + statusServico.Result.XMotivo);
+                this.textBox_resultado.Text = statusServico.Result.CStat + " " + statusServico.Result.XMotivo;
+
+            }
+            catch (Exception ex)
             {
-                TipoDFe = TipoDFe.NFe,
-                TipoEmissao = TipoEmissao.Normal,
-                CertificadoDigital = CertificadoSelecionado
-            };
 
-            var statusServico = new StatusServico(xml, configuracao);
-
-            statusServico.Executar();
-
-            MessageBox.Show(statusServico.Result.CStat + " " + statusServico.Result.XMotivo);
+                this.textBox_resultado.Text = ex.Message;
+            }
 
         }
 
@@ -62,6 +84,38 @@ namespace TreinamentoDLL
         /// <param name="e"></param>
         private void btnConsultaSituacao_Click(object sender, EventArgs e)
         {
+            this.textBox_resultado.Text = "";
+
+            try
+            {
+
+                var xml = new ConsSitNFe
+                {
+                    Versao = "4.00",
+                    TpAmb = TipoAmbiente.Homologacao,
+                    ChNFe = "13220404818357000132550040000026161900128580"
+                };
+
+                var configuracao = new Configuracao
+                {
+                    TipoDFe = TipoDFe.NFe,
+                    TipoEmissao = TipoEmissao.Normal,
+                    CertificadoDigital = CertificadoSelecionado
+                };
+
+                var consultaProtocolo = new ConsultaProtocolo(xml, configuracao);
+
+                consultaProtocolo.Executar();
+
+                //MessageBox.Show(consultaProtocolo.Result.CStat + " " + consultaProtocolo.Result.XMotivo);
+                this.textBox_resultado.Text = consultaProtocolo.Result.CStat + " " + consultaProtocolo.Result.XMotivo;
+
+            }
+            catch (Exception ex)
+            {
+
+                this.textBox_resultado.Text = ex.Message;
+            }
 
         }
 
@@ -70,18 +124,60 @@ namespace TreinamentoDLL
         /// <summary>
         /// Caminho Completo do Certificado
         /// </summary>
-        private static string CaminhoCertificadoDigital { get; set; } = @"C:\Dropbox\AGROPAM\AGROPAM 2022.pfx";
-        
+        private static string CaminhoCertificadoDigital; //{ get; set; } = @"C:\Dropbox\MULTBEEF\MULTBEEF.pfx";
+
+        /// <summary>
+        /// Deixa escolher o arquivo pfx e atualiza CaminhoCertificadoDigital
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxArqCert_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                using (OpenFileDialog dlg = new OpenFileDialog())
+                {
+                    dlg.RestoreDirectory = true;
+                    dlg.Filter = "Arquivos pfx|*.pfx";
+
+                    if (dlg.ShowDialog() == DialogResult.OK)
+                    {
+                        if (!string.IsNullOrEmpty(dlg.FileName))
+                        {
+                            this.textBoxArqCert.Text = dlg.FileName;
+                            CaminhoCertificadoDigital = this.textBoxArqCert.Text;
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Atualiza SenhaCertificadoDigital pelo textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxSenhaCert_TextChanged(object sender, EventArgs e)
+        {
+            SenhaCertificadoDigital = this.textBoxSenhaCert.Text;
+            CertificadoSelecionadoCampo = null;
+        }
+
         /// <summary>
         /// Senha do Certificado
         /// </summary>
-        private static string SenhaCertificadoDigital { get; set; } = "senhaadescobrir";
+        private static string SenhaCertificadoDigital; //{ get; set; } = "ALGUMASENHA";
 
         /// <summary>
         /// Campo para o Certificado Selecionado
         /// </summary>
         private static X509Certificate2 CertificadoSelecionadoCampo;
 
+        /// <summary>
+        /// Campo para o Certificado Selecionado
+        /// </summary>
         public static X509Certificate2 CertificadoSelecionado 
         {
             get 
@@ -97,8 +193,6 @@ namespace TreinamentoDLL
             private set => throw new Exception("Não é possível atribuir um certificado digital ! Somente resgate o valor da propriedade que o certificado é definido automaticamente.");
         }
 
-
         #endregion
-
     }
 }
